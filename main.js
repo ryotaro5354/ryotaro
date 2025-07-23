6a67bf6a1deba8dd01a33934019cfbbd1369a241
ryotaro <Tokudome.ryo.5354@outlook.jp>
2025-07-23 21:07:43 +0900

    img追加,js追加しています

---
+++ main.js
@@ -0,0 +1,42 @@
+
+/*-------------------------------------
+スムーススクロール
+-----------------------------------*/
+
+$(function () {
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr("href");
+        let target = $(href === "#" || href === "" ? "html" : href);
+        let position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, 400, "swing");
+        return false;
+    });
+});
+
+
+ScrollReveal({
+    reset: false,
+    distance: "100px",
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.main-title', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.info-title', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.mv', { delay: 700, origin: 'right' });
+
+ScrollReveal().reveal('.section-title', { delay: 150, origin: 'top' });
+ScrollReveal().reveal('.feature-text01', { delay: 150, origin: 'left' });
+ScrollReveal().reveal('.feature-text02', { delay: 150, origin: 'right' });
+ScrollReveal().reveal('.feature-text03', { delay: 150, origin: 'left' });
+ScrollReveal().reveal('.feature-text04', { delay: 150, origin: 'right' });
+
+ScrollReveal().reveal('.feature-img01', { delay: 150, origin: 'right' });
+ScrollReveal().reveal('.feature-img02', { delay: 150, origin: 'left' });
+ScrollReveal().reveal('.feature-img03', { delay: 150, origin: 'right' });
+ScrollReveal().reveal('.feature-img04', { delay: 150, origin: 'left' });
+
+ScrollReveal().reveal('.function-text', { delay: 150, origin: 'top' });
+ScrollReveal().reveal('.function-img', { delay: 150, origin: 'bottom' });
+
+ScrollReveal().reveal('.detail-text', { delay: 150, origin: 'top' });
+ScrollReveal().reveal('.detail-img', { delay: 150, origin: 'bottom' });
